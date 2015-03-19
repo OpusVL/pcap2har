ada7328deaebcfb3de55a7187390a8e87304b670
Colin Newell <colin@opusvl.com>
2015-03-19 16:35:21 +0000

    Extract contents of body from request as well as response

--- pcap2har/http/request.py
+++ pcap2har/http/request.py
@@ -2,10 +2,13 @@ import urlparse
 
 # dpkt.http is buggy, so we use our modified replacement
 from .. import dpkt_http_replacement as dpkt_http
+from .. import settings
+from ..mediatype import MediaType
 import message as http
+from response import Response
 
 
-class Request(http.Message):
+class Request(Response):
     '''
     HTTP request. Parses higher-level info out of dpkt.http.Request
     Members:
@@ -24,3 +27,20 @@ class Request(http.Message):
         self.fullurl = fullurl.geturl()
         self.url, frag = urlparse.urldefrag(self.fullurl)
         self.query = urlparse.parse_qs(uri.query, keep_blank_values=True)
+
+        if 'content-type' in self.msg.headers:
+            self.mediaType = MediaType(self.msg.headers['content-type'])
+        else:
+            self.mediaType = MediaType('application/x-unknown-content-type')
+        self.mimeType = self.mediaType.mimeType()
+        self.body_length = len(self.msg.body)
+        self.compression_amount = None
+        self.text = None
+        # handle body stuff
+        if settings.drop_bodies:
+            self.clear_body()
+        else:
+            # uncompress body if necessary
+            self.handle_compression()
+            # try to get out unicode
+            self.handle_text()
